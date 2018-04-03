598a47eaacc0ef859cc196b7504aecff7cecae9f
usimonline <usim.online@mail.ru>
2018-04-03 13:16:59 +0300

    1

---
+++ requisites.php
@@ -0,0 +1,14 @@
+<?php
+
+date_default_timezone_set('Europe/Moscow');
+$datetime_site = date("Y-m-d H:i:s");
+
+$main_name = "https://by-by.info";
+$main_name_corot = "By-By.info";
+$site_name = "By-By.info";
+$email = "contact@by-by.info";
+
+$name_user = 'u664223845_1';//база данных
+$Name_database = 'u664223845_1';
+$password = '111111';
+$name_server = 'mysql.hostinger.ru';
