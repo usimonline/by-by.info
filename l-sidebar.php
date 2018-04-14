4549a8d7bc0b77ce6a4b8a06c4a1935e4b9a9c20
usimonline <usim.online@mail.ru>
2018-04-14 22:23:51 +0300

    1

--- l-sidebar.php
+++ l-sidebar.php
@@ -72,7 +72,6 @@
 
 						<ul>
 							<li>
-								<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100025369361064&tabs&width=0&height=0&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=898599363654965" width="0" height="0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 							</li>
 						</ul>
 
