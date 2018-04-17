46fb99a727646ec9efacb261a40f7fb6faa88477
usimonline <usim.online@mail.ru>
2018-04-17 16:46:08 +0300

    1

--- sitemap.php
+++ sitemap.php
@@ -12,7 +12,7 @@
                 $n_l_u = $news_latest[$i]['url'];
                 $n_l_date = DateTime::createFromFormat('Y-m-d H:i:s', $news_latest[$i]['datetime'])->format('Y-m-d\TH:i:sP');
             $n_l_u_mass = explode( '/', $n_l_u);
-            if($n_l_u_mass[1] == '2018' or $n_l_u_mass[1] == '2019' ) {
+            if($n_l_u_mass[2] == '2018' or $n_l_u_mass[2] == '2019' ) {
                 $rss_file = $rss_file . '
                 <url>
                 <loc>' . $main_name . $n_l_u . '</loc>
