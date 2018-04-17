ac72f3ea55b8339f2841bce6060642f953d80328
usimonline <usim.online@mail.ru>
2018-04-17 16:02:11 +0300

    1

--- sitemap.php
+++ sitemap.php
@@ -9,14 +9,17 @@
 
 
         for($i = 0; $i < $total; $i++) {
-            $n_l_u = $news_latest[$i]['url'];
-            $n_l_date = DateTime::createFromFormat('Y-m-d H:i:s', $news_latest[$i]['datetime'])->format('Y-m-d\TH:i:sP');
-$rss_file = $rss_file.'
-<url>  
-            <loc>'.$main_name.$n_l_u.'</loc>
-            <lastmod>'.$n_l_date.'</lastmod>
-            <priority>0.8</priority>          
-</url>';
+                $n_l_u = $news_latest[$i]['url'];
+                $n_l_date = DateTime::createFromFormat('Y-m-d H:i:s', $news_latest[$i]['datetime'])->format('Y-m-d\TH:i:sP');
+            $n_l_u_mass = explode( '/', $n_l_u);
+            if($n_l_u_mass[2] == '2018' or $n_l_u_mass[2] == '2019' ) {
+                $rss_file = $rss_file . '
+                <url>
+                <loc>' . $main_name . $n_l_u . '</loc>
+                <lastmod>' . $n_l_date . '</lastmod>
+                <priority>0.8</priority>
+                </url>';
+            }
 }
 
 $rss_file = $rss_file.'
