e5b5adb3cc7741123c0fce58a1f52163b4f0f81d
usimonline <usim.online@mail.ru>
2018-04-17 16:19:17 +0300

    1

--- rss.php
+++ rss.php
@@ -9,32 +9,41 @@
             '.$site_name.' :: Новости Беларуси, Белорусские новости, Новости Белоруссии, News from Belarus
         </description>
         <image>
+        <url>'.$main_name.'/img/metro.jpg
+        </url>
             <title>
                 '.$site_name.' :: Новости Беларуси - Белорусские новости - Новости Белоруссии - Республика Беларусь - Минск
             </title>
-            <url>'.$main_name.'/img/metro.jpg</url>
             <link>'.$main_name.'</link>
-            <description>
-                '.$site_name.' :: Новости Беларуси, Белорусские новости, Новости Белоруссии, News from Belarus
-            </description>
-        </image>
 
-        <language>ru</language>';
+        <pubDate>Tue, 17 Apr 2018 16:02:25 +0300</pubDate>
+	<lastBuildDate>Tue, 17 Apr 2018 16:01:21 +0300</lastBuildDate>
+	<ttl>10</ttl>
+	<atom:link href="https://by-by.info/rss.xml" rel="self" type="application/rss+xml" />
+        ';
 
 
 
         for($i = 0; $i < $total; $i++) {
             $n_l_t =  $news_latest[$i]['teme'];
             $n_l_u = $news_latest[$i]['url'];
+            $n_l_u_pict = str_replace('news','pictures',$n_l_u).'/img_1.jpg';
             $n_l_date = DateTime::createFromFormat('Y-m-d H:i:s', $news_latest[$i]['datetime'])->format(DateTime::RSS);
             $n_l_des = $news_latest[$i]['description'];
 $rss_file = $rss_file.'
 <item turbo="true">
             <title>'.$n_l_t.'</title>
             <link>'.$main_name.$n_l_u.'</link>
-            <category>news</category>   
-            <pubDate>'.$n_l_date.'</pubDate>
             <description>'.$n_l_des.'</description> 
+            <atom:author>
+			<atom:name>By-by.info</atom:name>
+			<atom:uri>https://by-by.info/news</atom:uri>
+			</atom:author>    
+            <category domain="https://by-by.info/news">news</category>
+            <enclosure url="'.$main_name.$n_l_u_pict.'" type="image/jpeg" length="309999" />
+            <guid isPermaLink="true">'.$main_name.$n_l_u.'</guid>
+            <pubDate>'.$n_l_date.'</pubDate>
+            <media:content url="'.$main_name.$n_l_u_pict.'" type="image/jpeg" medium="image" height="785" width="1200" fileSize="309999" />         
 </item>';
 }
 
