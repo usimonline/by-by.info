d0dc203b06894db790c655525edeeb43a9dd6417
usimonline <usim.online@mail.ru>
2018-05-02 20:22:48 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = "https://by-by.info";
+$main_name = 'http://ffggddhh.esy.es';//"https://by-by.info";
 $main_name_corot = "By-By.info";
 $site_name = "By-By.info";
 $email = "contact@by-by.info";
