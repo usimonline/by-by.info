4dec386c39fc6a15cb135333e9ea3bcb245e41c3
usimonline <usim.online@mail.ru>
2018-05-02 22:18:29 +0300

    1

--- chek_form.php
+++ chek_form.php
@@ -1,5 +1,5 @@
 <h1>Please enter login and password</h1>
-<form method="POST" enctype="multipart/form-data" action="<?php echo $main_name; ?>/admin/empty/10">
+<form method="POST" enctype="multipart/form-data" action="<?php echo $main_name; ?>/admin/empty/10/">
 <p>Login:</p>
 <input type="text" name="login" />
 <p>Password:</p>
