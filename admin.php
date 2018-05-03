2b3da63418c4c271ecf82b173f9fee7b10c5ad0a
usimonline <usim.online@mail.ru>
2018-05-03 03:11:27 +0300

    1

--- admin.php
+++ admin.php
@@ -40,6 +40,36 @@ $teme_int = $_POST["teme_int"];
 	//echo '<br>';
 	//print_r($_FILES);
 	//Для загрузки файлов
+	$filePath  = $_FILES['filename']['tmp_name'];
+	$errorCode = $_FILES['filename']['error'];
+
+	if ($errorCode !== UPLOAD_ERR_OK || !is_uploaded_file($filePath)) {
+
+		// Массив с названиями ошибок
+		$errorMessages = [
+			UPLOAD_ERR_INI_SIZE   => 'Размер файла превысил значение upload_max_filesize в конфигурации PHP.',
+			UPLOAD_ERR_FORM_SIZE  => 'Размер загружаемого файла превысил значение MAX_FILE_SIZE в HTML-форме.',
+			UPLOAD_ERR_PARTIAL    => 'Загружаемый файл был получен только частично.',
+			UPLOAD_ERR_NO_FILE    => 'Файл не был загружен.',
+			UPLOAD_ERR_NO_TMP_DIR => 'Отсутствует временная папка.',
+			UPLOAD_ERR_CANT_WRITE => 'Не удалось записать файл на диск.',
+			UPLOAD_ERR_EXTENSION  => 'PHP-расширение остановило загрузку файла.',
+		];
+
+		// Зададим неизвестную ошибку
+		$unknownMessage = 'При загрузке файла произошла неизвестная ошибка.';
+
+		// Если в массиве нет кода ошибки, скажем, что ошибка неизвестна
+		$outputMessage = isset($errorMessages[$errorCode]) ? $errorMessages[$errorCode] : $unknownMessage;
+
+		// Выведем название ошибки
+		die($outputMessage);
+	}
+
+
+
+
+
 	if($_FILES['filename']['size'] > 1024*1024)
     {
     echo ("Размер файла превышает 1 мегабайт");
