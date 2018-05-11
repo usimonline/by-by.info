44d419ad0c94bfa8229f3614ec37cfd9a007ea36
usimonline <usim.online@mail.ru>
2018-05-11 19:55:20 +0300

    1

--- page.php
+++ page.php
@@ -33,10 +33,11 @@
 		   <?php
 	   }
 			if($news_year_2 == 0) {
-				//$string_page_temp = '<p>'.str_replace("<br><br>", '</p><p>', $page['text']).'</p>';
+				$string_page_temp = nl2br($page['text']);
+				$string_page_temp = '<p>'.str_replace("<br /><br />", '</p><p>', $string_page_temp).'</p>';
 				//$string_page_temp = str_replace("\n", '<br>', $string_page_temp);
-				//echo $string_page_temp;
-				echo '<p>'.nl2br($page['text']).'</p>';
+				echo $string_page_temp;
+				//echo '<p>'.nl2br($page['text']).'</p>';
 
 			} else echo $page['text'];
 			?>
