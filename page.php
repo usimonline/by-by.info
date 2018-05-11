599435d14c4b34acbd319bf9550e459505697d60
usimonline <usim.online@mail.ru>
2018-05-11 20:00:00 +0300

    1

--- page.php
+++ page.php
@@ -34,7 +34,8 @@
 	   }
 			if($news_year_2 == 0) {
 				$string_page_temp = nl2br($page['text']);
-				$string_page_temp = '<p>'.str_replace("<br />\r<br />", '</p><p>', $string_page_temp).'</p>';
+				$string_page_temp = '<p>'.str_replace("<br />\r\n<br />", '</p><p>', $string_page_temp).'</p>';
+				$string_page_temp = '<p>'.str_replace("<br />\n\r<br />", '</p><p>', $string_page_temp).'</p>';
 				//$string_page_temp = str_replace("\n", '<br>', $string_page_temp);
 				echo $string_page_temp;
 				//echo '<p>'.nl2br($page['text']).'</p>';
