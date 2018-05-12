59df8c872355bf1fdced11e130b4e1b23c578e88
usimonline <usim.online@mail.ru>
2018-05-12 18:20:24 +0300

    1

--- head.php
+++ head.php
@@ -46,8 +46,8 @@ if ($head_nomer_url_mass_2 == '2018-2') echo '<meta name="robots" content="noind
 <meta name="twitter:title" content="<?php echo $title; ?>" />
 <meta name="twitter:description" content="<?php echo $description ?>" />
 <meta name="twitter:image:src" content="<?php echo $main_name; ?><?php echo $url_og_picture; ?>" />
-<meta name="description" content="<?php echo $description ?>" >
-<meta name="keywords" content="<?php echo $keys; ?>">
+<meta name="description" content="<?php echo $description ?>" />
+<meta name="keywords" content="<?php echo $keys; ?>" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="<?php echo $main_name; ?><?php echo $url_og; ?>" />
 <meta property="og:title" content="<?php echo $site_name; ?> :: <?php echo $title; ?>" />
@@ -57,11 +57,11 @@ if ($head_nomer_url_mass_2 == '2018-2') echo '<meta name="robots" content="noind
 <meta property="og:image:height" content="249" />
 <meta name="title" content="<?php echo $title; ?>" />
 
-	<link rel="stylesheet" media="screen" href="<?php echo $main_name; ?>/index_files/screen.css">
-	<link rel="stylesheet" media="screen" href="<?php echo $main_name; ?>/index_files/screen-fix.css">
+	<link rel="stylesheet" media="screen" href="<?php echo $main_name; ?>/index_files/screen.css" />
+	<link rel="stylesheet" media="screen" href="<?php echo $main_name; ?>/index_files/screen-fix.css" />
 	<!--[if IE]><script src="/js/html5shiv.js"></script><![endif]-->
 
-<link rel="stylesheet" media="screen" href="<?php echo $main_name; ?>/index_files/classic-081711.css">
+<link rel="stylesheet" media="screen" href="<?php echo $main_name; ?>/index_files/classic-081711.css" />
 <script  src="<?php echo $main_name; ?>/index_files/mUjileCiCX-zdqBJO6mmFaJtiV4m78ixEoiEZmr9IDM.js"></script>
 <script src="<?php echo $main_name; ?>/index_files/remote.js"></script>
 <script  src="<?php echo $main_name; ?>/index_files/watch.js"></script>
