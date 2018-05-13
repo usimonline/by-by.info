9481bd3d6910cdd17ff72966e0989fb000915a0a
usimonline <usim.online@mail.ru>
2018-05-13 19:36:32 +0300

    1

--- functions.php
+++ functions.php
@@ -68,10 +68,10 @@ function transform_img($string,$url,$pic_alt) {
 	$second = '.jpg" alt="';
 	$third = '" /></div><figcaption>';
 	$fourth = '</figcaption></figure><p></p><p>';
-	if (!empty($pic_alt[2])) $string = str_replace("<img_2img>", $first.'2'.$second.'$pic_alt[2]'.$third.'$pic_alt[2]'.$fourth, $string);
-	if (!empty($pic_alt[3])) $string = str_replace("<img_3img>", $first.'3'.$second.'$pic_alt[3]'.$third.'$pic_alt[3]'.$fourth, $string);
-	if (!empty($pic_alt[4])) $string = str_replace("<img_4img>", $first.'4'.$second.'$pic_alt[4]'.$third.'$pic_alt[4]'.$fourth, $string);
-	if (!empty($pic_alt[5])) $string = str_replace("<img_5img>", $first.'5'.$second.'$pic_alt[5]'.$third.'$pic_alt[5]'.$fourth, $string);
+	if (!empty($pic_alt[2])) $string = str_replace("<img_2img>", $first.'2'.$second.$pic_alt[2].$third.$pic_alt[2].$fourth, $string);
+	if (!empty($pic_alt[3])) $string = str_replace("<img_3img>", $first.'3'.$second.$pic_alt[3].$third.$pic_alt[3].$fourth, $string);
+	if (!empty($pic_alt[4])) $string = str_replace("<img_4img>", $first.'4'.$second.$pic_alt[4].$third.$pic_alt[4].$fourth, $string);
+	if (!empty($pic_alt[5])) $string = str_replace("<img_5img>", $first.'5'.$second.$pic_alt[5].$third.$pic_alt[5].$fourth, $string);
 	//$img=array('<figure class="article__video"><div class="article__video-container"><img src="'.$url.'img_','.jpg" alt="Альтернатива" /></div><figcaption>Подпись под фото</figcaption></figure><p></p><p>');
 	//$zamena=array('<img_','img>');
 	//$string = str_replace($zamena, $img, $string);
