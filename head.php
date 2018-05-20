75eb52b2bfbaeb8177b50704c03b28b4a2e954aa
usimonline <usim.online@mail.ru>
2018-05-20 11:40:23 +0300

    1

--- head.php
+++ head.php
@@ -29,6 +29,7 @@ if ($head_nomer_url_mass_2 == '2018-2') echo '<meta name="robots" content="noind
 <meta name="yandex-verification" content="1a44c3037c640d63" />
 <meta name="google-site-verification" content="wUwLaGLrSvEUwUO380Lw1wTJ1mSMSy5aloNlqG7Eg80" />
 <meta name="p:domain_verify" content="528be2d34b6e17fe888a1fd12e3b9cea"/>
+<meta name="msvalidate.01" content="CC00204FB12476369B0C2CC2410806BA" />
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118296803-1"></script>
 <script>
