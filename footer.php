35a2d0e8c2a7efd750dd22c12eacb01674d7f94a
usimonline <usim.online@mail.ru>
2018-05-20 11:50:21 +0300

    1

--- footer.php
+++ footer.php
@@ -167,6 +167,21 @@
 			<img src="//counter.rambler.ru/top100.cnt?pid=6187417" alt="Топ-100" />
 		</noscript>
 		<!-- END Top100 (Kraken) Counter -->
+		<!-- Rating@Mail.ru counter -->
+		<script type="text/javascript">
+			var _tmr = window._tmr || (window._tmr = []);
+			_tmr.push({id: "3030977", type: "pageView", start: (new Date()).getTime()});
+			(function (d, w, id) {
+				if (d.getElementById(id)) return;
+				var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+				ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+				var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+				if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+			})(document, window, "topmailru-code");
+		</script><noscript><div>
+				<img src="//top-fwz1.mail.ru/counter?id=3030977;js=na" style="border:0;position:absolute;left:-9999px;" alt="" />
+			</div></noscript>
+		<!-- //Rating@Mail.ru counter -->
 	</div>
 </div>
 </footer>
