fadeaf4abf9f0bd9188f6cea10d618db3d03506c
usimonline <usim.online@mail.ru>
2018-06-03 11:38:30 +0300

    1

--- parser_insert_news.php
+++ parser_insert_news.php
@@ -43,6 +43,8 @@ $description = $url_mass_description[$k];
 $text = $url_mass_texts[$k];
 $text = transform_img($text, $url);
     $text = transform_words($text);
+    $text = str_replace("<br />\r\n<br />", '', $text);//это только для tut.by!!
+    $text = str_replace("<br />\n\r<br />", '', $text);//это только для tut.by!!
 
 $keys = $keys_temp;
 
