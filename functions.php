f1c6c87a3c60ad2e7def2bcb1afa567c5d892586
usimonline <usim.online@mail.ru>
2018-06-19 18:36:57 +0300

    1

--- functions.php
+++ functions.php
@@ -192,6 +192,8 @@ function podstanovka_kh2($string, $kol)
 	//for($i = 0; $i < $kol; $i++) {
 	$i = 0;
 	$string = preg_replace('[<h2>]', '<h2 id="my_page_'.$i.'">', $string, 1);
+	$i = 1;
+	$string = preg_replace('[<h2>]', '<h2 id="my_page_'.$i.'">', $string, 1);
 
 	//}
 	$string = str_replace('<<','<',$string);
