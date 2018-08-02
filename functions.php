5839286a14dd14396775e650b48fef6723b0f024
usimonline <usim.online@mail.ru>
2018-08-02 09:33:27 +0300

    1

--- functions.php
+++ functions.php
@@ -97,14 +97,14 @@ return $string;
 
 
 function transform_words($string) {
-	$string = str_replace('Беларусью','Белоруссией',$string);
-	$string = str_replace('через Беларусь','через Белоруссию',$string);
-	$string = str_replace('Через Беларусь','Через Белоруссию',$string);
-	$string = str_replace('Беларусью','Белоруссией',$string);
-	$string = str_replace('в Беларусь','в Белоруссию',$string);
-	$string = str_replace('В Беларусь','В Белоруссию',$string);
-	$string = str_replace('Беларусь','Белоруссия',$string);
-	$string = str_replace('Беларуси','Белоруссии',$string);
+	//$string = str_replace('Беларусью','Белоруссией',$string);
+	//$string = str_replace('через Беларусь','через Белоруссию',$string);
+	//$string = str_replace('Через Беларусь','Через Белоруссию',$string);
+	//$string = str_replace('Беларусью','Белоруссией',$string);
+	//$string = str_replace('в Беларусь','в Белоруссию',$string);
+	//$string = str_replace('В Беларусь','В Белоруссию',$string);
+	//$string = str_replace('Беларусь','Белоруссия',$string);
+	//$string = str_replace('Беларуси','Белоруссии',$string);
 	$string = str_replace('в Украине','на Украине',$string);
 	$string = str_replace('В Украине','На Украине',$string);
 	return $string;
