4cec405f39db73f3f4e356a5b20e507218c0047b
usimonline <usim.online@mail.ru>
2018-08-31 14:45:44 +0300

    1

--- requisites.php
+++ requisites.php
@@ -8,7 +8,7 @@ $main_name_corot = "By-By.info";
 $site_name = "By-By.info";
 $email = "contact@by-by.info";
 $site_url = $main_name."/";
-$password = '111qwaszx';
+$password = 'FhvsSdxCz16H';
 
 $name_user = 'u664223845_1';//база данных
 $Name_database = 'u664223845_1';
