fbe46bba7311405baf0c1d6cec9bf0896a8ce71d
usimonline <usim.online@mail.ru>
2018-10-21 17:04:32 +0300

    1

--- functions.php
+++ functions.php
@@ -198,7 +198,7 @@ function podstanovka_kh2($string, $kol)
 function beer_data($nomer_novosti, $all_zapisey){
 	$hernia = $all_zapisey - $nomer_novosti;
 	$time_temp = date("U");
-	$suka = $time_temp % 3600;
+	$suka = $time_temp % 3600*24;
 	$time_temp_2 = $time_temp - $suka -($hernia - 1)*240*60;
 	$result_data = date("Y-m-d H:i:s",$time_temp_2);
 	return $result_data;
