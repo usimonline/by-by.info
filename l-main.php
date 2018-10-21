45026ddb63c2dafd80469f38ddb1526991bd7578
usimonline <usim.online@mail.ru>
2018-10-21 12:56:18 +0300

    1

--- l-main.php
+++ l-main.php
@@ -46,7 +46,7 @@
 						$insert = "UPDATE $Name_database.$table SET nomer_novosti = $all_count_nomer WHERE datetime='$datetime_page_temp'";
 						$res = mysqli_query($link, $insert);
 
-					} else "Из базы данных ".$news_latest[$i]['nomer_novosti'];
+					} else "Из базы данных ".$news_latest[$count]['nomer_novosti'];
 
 
 
