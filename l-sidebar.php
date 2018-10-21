780334c68b63e31fefae7d890001b6ec3467a581
usimonline <usim.online@mail.ru>
2018-10-21 17:23:47 +0300

    1

--- l-sidebar.php
+++ l-sidebar.php
@@ -72,14 +72,14 @@
 							</ul>
 							<ul>
 
-								<li>
+								<!-- <li>
 
 
 									<a class="twitter-timeline"  href="https://twitter.com/search?q=%40byby_info" data-widget-id="991931261655617537" data-chrome="nofooter" lang="ru" rel="nofollow" target="_blank">Твиты про @byby_info</a>
 									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 
-								</li>
+								</li> -->
 							</ul>
 
 
