0380802eb34653eaa35d04da3a6eca7377e82cbf
usimonline <usim.online@mail.ru>
2018-10-24 11:40:22 +0300

    1

--- route.php
+++ route.php
@@ -448,7 +448,7 @@ while($row = mysqli_fetch_array($res))
 	$news_latest[$i]['description'] = $row['description'];
 	$news_latest[$i]['comments'] = $row['comments'];
 	$news_latest[$i]['nomer_novosti'] = $row['nomer_novosti'];
-	
+	$news_latest[$i]['url_ext'] = $row['url_ext'];
 	$news_latest[$i++]['url'] = $row['url'];
 }
 $total = $i;
