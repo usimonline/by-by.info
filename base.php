a4d3ca9949901764faba6d6ddef598083c7ecaa6
usimonline <usim.online@mail.ru>
2019-08-16 16:53:45 +0300

    1

--- base.php
+++ base.php
@@ -32,63 +32,62 @@ $all_count_nomer = $row[0] + 1; // количество записей ВСЕХ
 //$insert = "UPDATE $Name_database.$table SET nomer_novosti = $all_count_nomer WHERE datetime='$datetime_page_temp'";
 
 //$res = mysqli_query($link, $insert);
+//  далее все закомментировать. это код для перемешивания статей
+//$select = "SELECT * FROM $Name_database.$table_save WHERE ind = 0";
+//$res = mysqli_query($link, $select);
+//$i = 0;
+//while ($row = mysqli_fetch_array($res)) {
+//	$date_save[$i]['date_save'] = $row['date_save'];
+//}
 
-$select = "SELECT * FROM $Name_database.$table_save WHERE ind = 0";
-$res = mysqli_query($link, $select);
-$i = 0;
-while ($row = mysqli_fetch_array($res)) {
-	$date_save[$i]['date_save'] = $row['date_save'];
-}
+//$date_save = $date_save[0]['date_save'];
+//$datetime_site_ymd = date("Y-m-d");
 
-$date_save = $date_save[0]['date_save'];
-$datetime_site_ymd = date("Y-m-d");
-//echo $date_save.'<br>';
-//echo $datetime_site_ymd;
 
 
 //новые выборки первых 6 элементов
-if ($date_save != $datetime_site_ymd) {
+//if ($date_save != $datetime_site_ymd) {
 
-	$select = "UPDATE $Name_database.$table_save SET date_save = '$datetime_site_ymd' WHERE ind = 0";
-	$res = mysqli_query($link, $select);
+//	$select = "UPDATE $Name_database.$table_save SET date_save = '$datetime_site_ymd' WHERE ind = 0";
+//	$res = mysqli_query($link, $select);
 
-	$select = "SELECT * FROM $Name_database.$table WHERE datetime < '$datetime_site' ORDER BY datetime DESC LIMIT 6";
-	$res = mysqli_query($link, $select);
+//	$select = "SELECT * FROM $Name_database.$table WHERE datetime < '$datetime_site' ORDER BY datetime DESC LIMIT 6";
+//	$res = mysqli_query($link, $select);
 
-	$i = 0;
-	while ($row = mysqli_fetch_array($res)) {
-		$news_first_temp[$i]['id'] = $row['id'];
-		$news_first_temp[$i]['datetime'] = $row['datetime'];
-		$news_first_temp[$i++]['nomer_novosti'] = $row['nomer_novosti'];
-	}
+//	$i = 0;
+//	while ($row = mysqli_fetch_array($res)) {
+//		$news_first_temp[$i]['id'] = $row['id'];
+//		$news_first_temp[$i]['datetime'] = $row['datetime'];
+//		$news_first_temp[$i++]['nomer_novosti'] = $row['nomer_novosti'];
+//	}
 //нужна функция, которая будет давать 6 случайных чисел от 1 до $all_count_nomer shest_rand_chisel($max)
 
-	$nomer_shest = shest_rand_chisel($all_count_nomer);
+//	$nomer_shest = shest_rand_chisel($all_count_nomer);
 
-$i = 0;
-while ($i < 6) {
-	$nomer_shest_temp = $nomer_shest[$i];
-	$select = "SELECT * FROM $Name_database.$table WHERE nomer_novosti = $nomer_shest_temp";
-	$res = mysqli_query($link, $select);
-	$row = mysqli_fetch_array($res);
+//$i = 0;
+//while ($i < 6) {
+//	$nomer_shest_temp = $nomer_shest[$i];
+//	$select = "SELECT * FROM $Name_database.$table WHERE nomer_novosti = $nomer_shest_temp";
+//	$res = mysqli_query($link, $select);
+//	$row = mysqli_fetch_array($res);
 
-	$news_first_temp_2[$i]['id'] = $row['id'];
-	$news_first_temp_2[$i]['datetime'] = $row['datetime'];
-	$news_first_temp_2[$i++]['nomer_novosti'] = $row['nomer_novosti'];
-}
-$i = 0;
-while ($i < 6) {
-	$per_base_temp_id = $news_first_temp[$i]['id'];
-	$per_base_temp_date = $news_first_temp[$i]['datetime'];// нужно поменять от 0 - самая первая до 5 - последняя по дате $datetime_site_ymd.$vramia_novosi;
-	$per_base_temp_nomer = $news_first_temp[$i]['nomer_novosti'];
-	$per_base_temp_id_rand = $news_first_temp_2[$i]['id'];
-	$per_base_temp_date_rand = $news_first_temp_2[$i]['datetime'];
-	$per_base_temp_nomer_rand = $news_first_temp_2[$i++]['nomer_novosti'];
+//	$news_first_temp_2[$i]['id'] = $row['id'];
+//	$news_first_temp_2[$i]['datetime'] = $row['datetime'];
+//	$news_first_temp_2[$i++]['nomer_novosti'] = $row['nomer_novosti'];
+//}
+//$i = 0;
+//while ($i < 6) {
+//	$per_base_temp_id = $news_first_temp[$i]['id'];
+//	$per_base_temp_date = $news_first_temp[$i]['datetime'];// нужно поменять от 0 - самая первая до 5 - последняя по дате $datetime_site_ymd.$vramia_novosi;
+//	$per_base_temp_nomer = $news_first_temp[$i]['nomer_novosti'];
+//	$per_base_temp_id_rand = $news_first_temp_2[$i]['id'];
+//	$per_base_temp_date_rand = $news_first_temp_2[$i]['datetime'];
+//	$per_base_temp_nomer_rand = $news_first_temp_2[$i++]['nomer_novosti'];
 
-	$select = "UPDATE $Name_database.$table SET nomer_novosti = $per_base_temp_nomer_rand, datetime = '$per_base_temp_date_rand' WHERE id = $per_base_temp_id";
-	$res = mysqli_query($link, $select);
-	$select = "UPDATE $Name_database.$table SET nomer_novosti = $per_base_temp_nomer, datetime = '$per_base_temp_date' WHERE id = $per_base_temp_id_rand";
-	$res = mysqli_query($link, $select);
-}
+//	$select = "UPDATE $Name_database.$table SET nomer_novosti = $per_base_temp_nomer_rand, datetime = '$per_base_temp_date_rand' WHERE id = $per_base_temp_id";
+//	$res = mysqli_query($link, $select);
+//	$select = "UPDATE $Name_database.$table SET nomer_novosti = $per_base_temp_nomer, datetime = '$per_base_temp_date' WHERE id = $per_base_temp_id_rand";
+//	$res = mysqli_query($link, $select);
+//}
 
-}
+//}
