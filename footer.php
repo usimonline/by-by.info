b4cc9240d514b0e75d8df38c3cfcd52b8b3f1d72
usimonline <usim.online@mail.ru>
2019-12-29 10:14:54 +0300

    1

--- footer.php
+++ footer.php
@@ -126,6 +126,16 @@
 		При использовании материалов сайта ссылка на сайт "<?php echo $site_name; ?>" обязательна.<br>
 		Сайт "<?php echo $site_name; ?>": <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
 		</p>
+		<!--LiveInternet counter--><script>
+			document.write("<a href='//www.liveinternet.ru/click' rel='nofollow'"+
+				"target=_blank><img src='//counter.yadro.ru/hit?t44.6;r"+
+				escape(document.referrer)+((typeof(screen)=="undefined")?"":
+				";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+					screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+				";h"+escape(document.title.substring(0,150))+";"+Math.random()+
+				"' alt='' title='LiveInternet' "+
+				"border='0' width='31' height='31'><\/a>")
+		</script><!--/LiveInternet-->
 		
 
 	</div>
