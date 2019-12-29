3ac9e5696b24452c0d82d9d287589ae79c113f5b
usimonline <usim.online@mail.ru>
2019-12-29 12:48:35 +0300

    1

--- header.php
+++ header.php
@@ -61,7 +61,8 @@
 	<div class="footer__topics">
 		<ul>
 			<li><a href="<?php echo $main_name; ?>/eaes/" >Евразийский Союз</a></li>
-			<li><a href="<?php echo $main_name; ?>/topic/toplist/50/" >Статьи</a></li>
+			<li><a href="<?php echo $main_name; ?>/topweek/" >События недели</a></li>
+			<li><a href="<?php echo $main_name; ?>/headernews/" >Аналитика</a></li>
 			<li><a href="<?php echo $main_name; ?>/centralnews/" >Центральные новости</a></li>
 			<li><a href="<?php echo $main_name; ?>/experts/" >Мнение экспертов</a></li>
 			<li><a href="<?php echo $main_name; ?>/coldwar/" >Холодная война</a></li>
