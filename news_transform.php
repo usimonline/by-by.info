137844fb5a3a0ca7229c05b0cb59aaf4b4c15043
usimonline <usim.online@mail.ru>
2020-02-12 20:50:58 +0300

    1

---
+++ news_transform.php
@@ -0,0 +1,24 @@
+﻿<?php
+if (!empty($_POST['article'])) {
+    $text = $_POST['article'];
+    $separator = '\r\n';
+
+    $mass = explode($separator, $text);
+
+
+
+    $final_mass = $mass; //array();
+    $final_text = implode ('!', $final_mass); // объединение с разделителем !
+}
+
+?>
+
+<form method="POST" enctype="multipart/form-data" action="<?php echo $main_name; ?>/news_transform.php">
+<textarea style="width:600px; height:25px; border: 1px solid #cccccc;" name="article" type="text" >Текст</textarea><br>
+
+    <input style="width:200px; height:50px; border: 1px solid #cccccc;" type="submit" value="Отправить статью"/>
+    <br><br>
+</form>
+
+
+
