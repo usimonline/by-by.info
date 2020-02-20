1c56fe36d2ea8413082997ca9d653230c2f3b93f
usimonline <usim.online@mail.ru>
2020-02-20 19:50:49 +0300

    1

--- news_transform.php
+++ news_transform.php
@@ -21,20 +21,20 @@ if (!empty($_POST['article'])) {
         $t++;
    }
 
-    $slova__ssilki = array(); // далее добавляю ссылки в текст
+    //$slova__ssilki = array(); // далее добавляю ссылки в текст
 
 
-    $spis_slov = array('США','Вьетнам', 'американский','американские','Соединенные Штаты');
-    $odna_tema = 'политика';
-    $poiasnenie = 'Преступления США во Вьетнаме, убийство мирных детей, женщин и стариков';
-    $url = '/coldwar/2020/01/25/1579957048/Kak-armija-SShA-povtorila-zverstva-fashistov-nad-mirnim-naseleniem-i-detmi-vo-Vetname/';
-    $slova_ssilki[] = array($spis_slov,$url,$odna_tema, $poiasnenie);
+    //$spis_slov = array('США','Вьетнам', 'американский','американские','Соединенные Штаты');
+    //$odna_tema = 'политика';
+    //$poiasnenie = 'Преступления США во Вьетнаме, убийство мирных детей, женщин и стариков';
+    //$url = '/coldwar/2020/01/25/1579957048/Kak-armija-SShA-povtorila-zverstva-fashistov-nad-mirnim-naseleniem-i-detmi-vo-Vetname/';
+   // $slova_ssilki[] = array($spis_slov,$url,$odna_tema, $poiasnenie);
 
-    $spis_slov = array('политика','Украина','украинский','бандеровцы','украинские');
-    $odna_tema = 'экономика';
-    $poiasnenie = 'Преступления Украины во времеа ВОВ, убийство мирных детей, женщин и стариков';
-    $url = '/agents/2020/01/23/1579761438/prestuplenija-ukrainskih-nacistov-v-godi-vojni/';
-    $slova_ssilki[] = array($spis_slov,$url,$odna_tema, $poiasnenie);
+    //$spis_slov = array('политика','Украина','украинский','бандеровцы','украинские');
+    //$odna_tema = 'экономика';
+    //$poiasnenie = 'Преступления Украины во времеа ВОВ, убийство мирных детей, женщин и стариков';
+    //$url = '/agents/2020/01/23/1579761438/prestuplenija-ukrainskih-nacistov-v-godi-vojni/';
+    //$slova_ssilki[] = array($spis_slov,$url,$odna_tema, $poiasnenie);
 
 
 
