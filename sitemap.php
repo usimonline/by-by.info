4fe32fd0e443cebd3106c3a11eeab7c5f938e17d
usimonline <usim.online@mail.ru>
2020-03-18 19:15:47 +0300

    1

--- sitemap.php
+++ sitemap.php
@@ -1,8 +1,10 @@
-<?php $rss_file = '<?xml version="1.0" encoding="UTF-8"?>
+<?php
+
+$rss_file = '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 <url>
-	<loc>https://by-by.info/news/</loc>
-	<lastmod>2018-04-14T20:11:26+01:00</lastmod>
+	<loc>https://by-by.info/</loc>
+	<lastmod>'.$datetime_site_schema_format.'</lastmod>
 	<priority>1</priority>
 </url>';
 
@@ -12,7 +14,7 @@
                 $n_l_u = $news_latest[$i]['url'];
                 $n_l_date = DateTime::createFromFormat('Y-m-d H:i:s', $news_latest[$i]['datetime'])->format('Y-m-d\TH:i:sP');
             $n_l_u_mass = explode( '/', $n_l_u);
-            if($n_l_u_mass[2] == '2018' or $n_l_u_mass[2] == '2019' ) {
+            if($n_l_u_mass[2] == '2018' or $n_l_u_mass[2] == '2019' or $n_l_u_mass[2] == '2020') {
                 $rss_file = $rss_file . '
                 <url>
                 <loc>' . $main_name . $n_l_u . '</loc>
