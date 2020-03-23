6d26526477ef5054adb48c446c265aaa533bc04a
usimonline <usim.online@mail.ru>
2020-03-23 14:23:29 +0300

    1

--- route.php
+++ route.php
@@ -137,7 +137,7 @@ if (empty($row)) {
 	$page['text'] = $row['text'];
 	$page['keys'] = $row['keys'];
 	$temp_rubrika_temp_sup = $page['keys'];
-	//$temp_rubrika_temp_sup = $temp_rubrika_temp_sup[0];
+	$temp_rubrika_temp_sup = $temp_rubrika_temp_sup[0];
 	print_r($temp_rubrika_temp_sup);
 	$page['id'] = $row['id'];
 	$page['title'] = $row['title'];
