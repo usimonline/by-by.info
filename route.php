f8d6f34406566307b2d166f69709fc4a40926bfb
usimonline <usim.online@mail.ru>
2020-03-23 14:19:19 +0300

    1

--- route.php
+++ route.php
@@ -138,6 +138,7 @@ if (empty($row)) {
 	$page['keys'] = $row['keys'];
 	$temp_rubrika_temp_sup = $page['keys'];
 	$temp_rubrika_temp_sup = $temp_rubrika_temp_sup[0];
+	echo $temp_rubrika_temp_sup;
 	$page['id'] = $row['id'];
 	$page['title'] = $row['title'];
 	$page['metadesc'] = $row['metadesc'];
@@ -173,8 +174,8 @@ $keys_name = 'keys';
 $keys_name_rubrika = 'keys';
 $keys_rubrika = '';
 if ($perikluch_page and $temp_rubrika_temp != 'news'){//срабатывает только для page
-	$keys_rubrika = translate_into_russian_pastnews($temp_rubrika_temp); // тут нужно вставлять рубрику не с помощью функции, а из базы данных
-	//$keys_rubrika = $temp_rubrika_temp_sup;
+	//$keys_rubrika = translate_into_russian_pastnews($temp_rubrika_temp); // тут нужно вставлять рубрику не с помощью функции, а из базы данных
+	$keys_rubrika = $temp_rubrika_temp_sup;
 }
 
 
