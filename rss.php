bfa2499acb1a6b2f0b23a7a2a902053542c89dd2
usimonline <111qwasZX>
2020-04-19 15:16:46 +0300

    1

--- rss.php
+++ rss.php
@@ -64,7 +64,7 @@ if($news_year_2 == 0) {
             <turbo:source></turbo:source>
             <turbo:topic>' . $n_l_t . '</turbo:topic>
             <pubDate>' . $n_l_date . '</pubDate>
-            <author>By-by.info</author>
+            <author>'.$autor_123.'</author>
             <yandex:related>
             <link url="'.$main_name .$url_int_234.'" >'.$teme_int_234.'</link>
             </yandex:related>
