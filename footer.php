ec29bde42e916bb15c13c72d39b4fc6d4dab8a27
usimonline <usim.online@mail.ru>
2021-02-06 21:59:07 +0300

    1

--- footer.php
+++ footer.php
@@ -17,64 +17,7 @@
 				"' alt='' title='LiveInternet' "+
 				"border='0' width='31' height='31'><\/a>")
 		</script><!--/LiveInternet-->
-		<!-- Top100 (Kraken) Counter -->
-		<script>
-			(function (w, d, c) {
-				(w[c] = w[c] || []).push(function() {
-					var options = {
-						project: 6187417,
-						trackHashes: true,
-						user_id: null,
-					};
-					try {
-						w.top100Counter = new top100(options);
-					} catch(e) { }
-				});
-				var n = d.getElementsByTagName("script")[0],
-					s = d.createElement("script"),
-					f = function () { n.parentNode.insertBefore(s, n); };
-				s.type = "text/javascript";
-				s.async = true;
-				s.src =
-					(d.location.protocol == "https:" ? "https:" : "http:") +
-					"//st.top100.ru/top100/top100.js";
-
-				if (w.opera == "[object Opera]") {
-					d.addEventListener("DOMContentLoaded", f, false);
-				} else { f(); }
-			})(window, document, "_top100q");
-		</script>
-		<noscript>
-			<img src="//counter.rambler.ru/top100.cnt?pid=6187417" alt="Топ-100" />
-		</noscript>
-		<!-- END Top100 (Kraken) Counter -->
-		<!-- Rating@Mail.ru counter -->
-		<script>
-			var _tmr = window._tmr || (window._tmr = []);
-			_tmr.push({id: "3030977", type: "pageView", start: (new Date()).getTime()});
-			(function (d, w, id) {
-				if (d.getElementById(id)) return;
-				var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-				ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
-				var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-				if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-			})(document, window, "topmailru-code");
-		</script><noscript><div>
-				<img src="//top-fwz1.mail.ru/counter?id=3030977;js=na" style="border:0;position:absolute;left:-9999px;" alt="" />
-			</div></noscript>
-		<!-- //Rating@Mail.ru counter -->
-
-		<!--Openstat-->
-		<span id="openstat1"></span>
-		<script>
-			var openstat = { counter: 1, next: openstat, track_links: "all" };
-			(function(d, t, p) {
-				var j = d.createElement(t); j.async = true; j.type = "text/javascript";
-				j.src = ("https:" == p ? "https:" : "http:") + "//openstat.net/cnt.js";
-				var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(j, s);
-			})(document, "script", document.location.protocol);
-		</script>
-		<!--/Openstat-->
+		
 
 		
 
